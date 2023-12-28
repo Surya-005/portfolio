64175fbe16a8606a3e6910606f4cf1f52afa5cbb
Surya-005 <suryac22012001@gmail.com>
2023-12-27 20:33:44 -0800

    trial

--- src/home.js
+++ src/home.js
@@ -1,28 +1,28 @@
 import React from "react";
 import "./home.css"
 
-export default function Home(){
-    return(
+export default function Home() {
+    return (
         <div>
-            <div>
-                <div>
+            <div className='landing'>
+                <div className="greetings">
                     <h1>hi buddy</h1>
                     <h1>welcome</h1>
+                    <div>
+                        <button>explore</button>
+                    </div>
                 </div>
-                <div>
+                <div className="intro_photo">
                     <div>photo</div>
                     <h1>surya</h1>
                     <h1>front end developer</h1>
                 </div>
-                <div>
-                    <button>explore</button>
-                </div>
             </div>
             <div>
-                <div>about</div>   
-                <div>contact</div>   
+                <div>about</div>
+                <div>contact</div>
                 <div>projects</div>
-                         
+
             </div>
         </div>
     )
